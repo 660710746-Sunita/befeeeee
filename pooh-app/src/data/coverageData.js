a07600c38d4c0c86d35d24b63a7d7376b192f8fd
Sunita Krittinnawong <krittinnawong_s@su.ac.th>
2025-11-18 15:58:35 +0000

    detail

---
+++ pooh-app/src/data/coverageData.js
@@ -0,0 +1,22 @@
+const coverageData = {
+  1: { // 2+
+    car: { damage: 100000, lost: 100000, fire: 100000, flood: 0 },
+    thirdParty: { person: 500000, accident: 10000000, property: 15000000, deductible: "-" },
+    extra: {personalAccident: "100,000 (1+6)",medicalExpense: "100,000 (1+6)",bail: "200,000"},
+    service: { condolence: "5,000 / ต่อคน", death: "10,000 / ต่อคน"}
+  },
+  2: { // 3+
+    car: { damage: 100000, lost: 0, fire: 0, flood: 0 },
+    thirdParty: { person: 500000, accident: 10000000, property: 150000000, deductible: "-" },
+    extra: {personalAccident: "100,000 (1+6)",medicalExpense: "100,000 (1+6)",bail: "200,000"},
+    service: { condolence: "-", death: "-"}
+  },
+  3: { // 3 ธรรมดา
+    car: { damage: 0, lost: 0, fire: 0, flood: 0 },
+    thirdParty: { person: 500000, accident: 10000000, property: 1000000, deductible: "-" },
+    extra: {personalAccident: "50,000 (1+5)",medicalExpense: "50,000 (1+5)",bail: "50,000"},
+    service: { condolence: "-", death: "-"}
+  }
+};
+
+export default coverageData;
